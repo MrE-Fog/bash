03dfd01a8d4993ac4eae71576767ce95e0cb0584
t <tristan.e.callahan@gmail.com>
2021-01-09 10:58:11 -0500

    Updated REAME.md and added additional error checking to checksum.sh

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # bash
+
+This listing contains multiple bash scripts.
+Some are for testing purposes, others are for convenience.
+
+#error_report.sh 
+
+This script does nothing but collect error messages from syslog and syslog.1 into a single file.
+
+#ping_sweep.sh
+
+Ping sweep looks for active nodes on a subnet.
+It is currently configured to 192.168.1.x but could easily be modified to use any other.
+
+#checksum.sh
+
+checksum.sh is a wrapper around the OpenSSL utility.
+If given a file and an algorithm it will return the hash of the file.
+If given a file, algorithm, and digest it will generate a new hash from the given file and compare it to the digest using the selected algorithm.
--- checksum.sh
+++ checksum.sh
@@ -1,8 +1,50 @@
 #!/bin/env bash
 
+#list of digest algorithms supported by OpenSSL
+algorithms=(
+	"blake2b512"
+	"blake2s256"
+	"gost"
+	"md4"
+	"md5"
+	"rmd160"
+	"sha1"
+	"sha224"
+	"sha256"
+	"sha3-224"
+	"sha3-256"
+	"sha3-384"
+	"sha3-512"
+	"sha384"
+	"sha512"
+	"sha512-224"
+	"sha512-256"
+	"shake128"
+	"shake256"
+	"sm3"
+)
+
+matchAglorithm() {
+	local e match="$1"
+	shift
+	for e; do [[ "$e" == "$match" ]] && return 0; done
+	return 1
+}
+
+printAlgorithms() {
+	echo "";
+	echo "The following algorithms are supported by OpenSSL";
+	for algo in "${algorithms[@]}"
+	do
+		echo "$algo";
+	done
+}
+
 #print usage help and exit
 usage() { echo "Usage: $0 [-f file_path] [-a algorithm] [-d digest to compare]" 1>&2;
-	  echo "Preferred algorithms are MD5, SHA3-256, and SHA3-512";
+	  echo "Preferred algorithms are MD5, SHA3-256, and SHA3-512" 1>&2;
+	  echo "";
+	  #printAlgorithms;
  	exit 1;
 }
 
@@ -13,13 +55,38 @@ do
 		f) filename=${OPTARG};;
 		d) digest=${OPTARG};;
 		a) algorithm=${OPTARG};;
-		h|?) usage exit 0;;
-		*) usage exit 1;;
+		h|?) usage;;
+		*) usage;;
 	esac
 done
 
 #check that command line args exist
-if [ -z "$1" ] || [ -z "$algorithm" ]
+if [[ -z $1 ]] || [[ -z $algorithm ]]
+then
+	usage;
+fi
+
+#see if supplied algorithm is on the OpenSSL supported digest list from openssl help
+found=false
+
+for i in "${algorithms[@]}"
+do
+	if [ "${i}" == "${algorithm,,}" ]
+	then
+		found=true;
+		break;
+	else
+		found=false;
+	fi
+done
+#if the algorithm choice wasn't found show help
+if [ ! $found ]
+then
+	usage;
+fi
+
+#check that input is a valid file. If not a file call usage function.
+if [[ ! -f $filename ]]
 then
 	usage;
 fi
@@ -28,7 +95,7 @@ fi
 hash=$(openssl dgst -$algorithm $filename | awk '{print $2}');
 
 #return hash of file if no digest is provided by user
-if [ -n "$algorithm" ] && [ -z "$digest" ]
+if [[ -n $algorithm ]] && [[ -z $digest ]]
 then
 	echo ""; echo "";
 	echo "The hash of this file is: $hash";
@@ -39,7 +106,7 @@ then
 fi
 
 #compare file hash with digest provided by user
-if [ "$hash" = "$digest" ]
+if [[ $hash = $digest ]]
 then
 	echo "";
 	echo "The hashes match! Data valid!";
@@ -51,10 +118,3 @@ else
 	echo "";
 	exit 0;
 fi
-
-
-#Debug and test output
-echo "$filename";
-echo "$digest";
-echo "$algorithm";
-echo "$hash";
